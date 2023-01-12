3ba0cad2dbdf92c885416628550103a2ff4ae3e7
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-01-12 07:48:08 +0530

    Setup tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import { Inter } from "@next/font/google";
+import "./globals.css";
+import clsx from "clsx";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className={clsx(inter.variable, "font-sans")}>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
