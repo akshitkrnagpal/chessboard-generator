1cd5778cdb7302bcca2cd8dfd27ded8ed54572c6
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-02-18 06:05:08 +0530

    Added next-pwa fork and manifest

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const withPWAInit = require("@ducanh2912/next-pwa").default;
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
